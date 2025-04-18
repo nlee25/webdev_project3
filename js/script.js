44484402d649d9ddb2f90b09f8dcf8d9ea785f1b
nlee25 <nlee25@hawk.iit.edu>
2025-04-18 15:32:05 -0500

    removal of scrap js

--- js/script.js
+++ js/script.js
@@ -36,3 +36,35 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 });
+
+
+function initMap() {
+  const location = { lat: 41.8781, lng: -87.6298 }; // Example coordinates (Chicago, IL)
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: location,
+  });
+
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: "Chicago",
+  });
+
+  const infoWindowContent = `
+    <div>
+      <h2>Disney Land</h2>
+      <p>Very fun.</p>
+      <a href="https://www.google.com/maps/dir/?api=1&destination=41.8781,-87.6298" target="_blank">Get Directions</a>
+    </div>
+  `;
+
+  const infoWindow = new google.maps.InfoWindow({
+    content: infoWindowContent,
+  });
+
+  marker.addListener("click", () => {
+    infoWindow.open(map, marker);
+  });
+}
